2e12172a41a91ad341d4314f83aa61f1e38fa8a1
Gabriel García Millán <gabriel.garciamillan01@gmail.com>
2024-04-25 12:49:20 +0200

    Initial Commit

---
+++ iBigBang/iBigBangApp.swift
@@ -0,0 +1,17 @@
+//
+//  iBigBangApp.swift
+//  iBigBang
+//
+//  Created by Gabriel Garcia Millan on 25/4/24.
+//
+
+import SwiftUI
+
+@main
+struct iBigBangApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
